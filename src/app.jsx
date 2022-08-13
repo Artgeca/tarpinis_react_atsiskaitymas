572099faa5a160e22383b2a6103b49803fba5861
artgeca <artgeca@gmail.com>
2022-08-13 13:02:28 +0300

    feat: added data to db

---
+++ src/app.jsx
@@ -0,0 +1,17 @@
+import { Box, Typography } from "@mui/material";
+
+const App = () => {
+  return (
+    <Box display='flex'
+      flexDirection='column'
+      alignItems='center'
+      sx={{bgcolor: 'grey.200', width: '100%', minHeight: '100vh', m: 0}}>
+        <Typography variant='h4' p={1}>Tickets</Typography>
+    {
+      
+    }
+    </Box>
+  );
+};
+
+export default App;
