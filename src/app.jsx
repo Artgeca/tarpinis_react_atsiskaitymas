8a3745cf12b75fa307e0c38c6e40762f8652d0ee
artgeca <artgeca@gmail.com>
2022-08-15 10:29:03 +0300

    feat: added content wrapper

--- src/app.jsx
+++ src/app.jsx
@@ -26,33 +26,39 @@ const App = () => {
   return (
     <Box
       display='flex'
-      flexDirection='column'
-      alignItems='center'
+      justifyContent='center'
       sx={{
         bgcolor: 'grey.200', width: '100%', minHeight: '100vh', m: 0,
       }}
     >
-      <Typography variant='h4' p={1} gutterBottom>Tickets</Typography>
       <Box
+        maxWidth={1400}
         display='flex'
-        justifyContent='center'
-        gap={2}
-        flexWrap='wrap'
+        flexDirection='column'
+        alignItems='center'
       >
-        {
+        <Typography variant='h4' p={1} gutterBottom>Tickets</Typography>
+        <Box
+          display='flex'
+          justifyContent='center'
+          gap={2}
+          flexWrap='wrap'
+        >
+          {
         tickets.map((ticket) => (
           <TicketCard key={ticket.id} {...ticket} onDelete={deleteTicket} />
         ))
         }
+        </Box>
+        <Button
+          variant='contained'
+          sx={{
+            position: 'absolute', bottom: 40, right: 40, height: '64px', width: '64px', borderRadius: '50%',
+          }}
+        >
+          <AddIcon />
+        </Button>
       </Box>
-      <Button
-        variant='contained'
-        sx={{
-          position: 'absolute', bottom: 40, right: 40, height: '64px', width: '64px', borderRadius: '50%',
-        }}
-      >
-        <AddIcon />
-      </Button>
     </Box>
   );
 };
